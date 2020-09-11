009f06cd9a8f2dc9a7a7bbc6e2b1a0466900caa0
super <super@super.com>
2020-09-11 15:41:55 +0900

    オブジェクトのuseState、フォームに入力された値を動的に管理する

--- react-basic/src/components/Basic.js
+++ react-basic/src/components/Basic.js
@@ -1,14 +1,25 @@
 import React, {useState}from 'react'
 
-const Basic = (props) => {
+const Basic = () => {
 
-  const [count, setCount] = useState(0)
+  const [product, setProducts] = useState({name:'', price:''})
 
 
   return (
-    <div>
-      <button onClick={() => setCount(count+1)}>Count{count}</button>
-    </div>
+    <>
+      <form>
+        <input type="text" value={product.name}
+          onChange={evt => setProducts({ ...product, name: evt.target.value })} />
+        
+        <input type="text" value={product.price}
+          onChange={evt => setProducts({...product, price: evt.target.value})}/>
+
+      </form>
+      
+      <h3>Product name is {product.name}</h3>
+      <h3>Product price is {product.price}</h3>
+      
+    </>
   )
 }
 
