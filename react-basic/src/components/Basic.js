f17a21ae7189b348f225df602ffe4c9b69610151
super <super@super.com>
2020-09-11 14:36:05 +0900

    クリックハンドラー

--- react-basic/src/components/Basic.js
+++ react-basic/src/components/Basic.js
@@ -1,8 +1,14 @@
 import React from 'react'
 
 const Basic = (props) => {
+
+  const clickHandler = () => {
+    console.log("clicked")
+  }
+
   return (
     <div>
+      <button onClick={clickHandler}>Click</button>
       <h1>I am {props.name}</h1>
       <h1>I am {props.age} years old</h1>
     </div>
